2c307c83d22e955a0b30fe8a546ad0ff6086e147
lazarevd <lazarevd@gmail.com>
2020-02-01 22:44:48 +0300

    1

---
+++ ConsoleCaptureOpenCV/ConsoleCaptureOpenCV.cpp
@@ -0,0 +1,86 @@
+#include "stdafx.h"
+#include "windows.h"
+#include "tchar.h"
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+#include <iostream>
+
+Mat hwnd2mat(HWND hwnd, int x, int y, int w, int h) {
+
+	HDC hwindowDC, hwindowCompatibleDC;
+
+	int height, width, srcheight, srcwidth;
+	HBITMAP hbwindow;
+	Mat src;
+	BITMAPINFOHEADER  bi;
+
+	hwindowDC = GetDC(hwnd);
+	hwindowCompatibleDC = CreateCompatibleDC(hwindowDC);
+	SetStretchBltMode(hwindowCompatibleDC, COLORONCOLOR);
+
+	RECT windowsize;    // get the height and width of the screen
+	GetClientRect(hwnd, &windowsize);
+
+	srcheight = windowsize.bottom;
+	srcwidth = windowsize.right;
+	height = windowsize.bottom / 2;  //change this to whatever size you want to resize to
+	width = windowsize.right / 2;
+
+	src.create(height, width, CV_8UC4);
+
+	// create a bitmap
+	hbwindow = CreateCompatibleBitmap(hwindowDC, width, height);
+	bi.biSize = sizeof(BITMAPINFOHEADER);    //http://msdn.microsoft.com/en-us/library/windows/window/dd183402%28v=vs.85%29.aspx
+	bi.biWidth = width;
+	bi.biHeight = -height;  //this is the line that makes it draw upside down or not
+	bi.biPlanes = 1;
+	bi.biBitCount = 32;
+	bi.biCompression = BI_RGB;
+	bi.biSizeImage = 0;
+	bi.biXPelsPerMeter = 0;
+	bi.biYPelsPerMeter = 0;
+	bi.biClrUsed = 0;
+	bi.biClrImportant = 0;
+
+	// use the previously created device context with the bitmap
+	SelectObject(hwindowCompatibleDC, hbwindow);
+	// copy from the window device context to the bitmap device context
+	StretchBlt(hwindowCompatibleDC, 0, 0, width, height, hwindowDC, 0, 0, srcwidth, srcheight, SRCCOPY); //change SRCCOPY to NOTSRCCOPY for wacky colors !
+	GetDIBits(hwindowCompatibleDC, hbwindow, 0, height, src.data, (BITMAPINFO *)&bi, DIB_RGB_COLORS);  //copy from hwindowCompatibleDC to hbwindow
+
+																									   // avoid memory leak
+	DeleteObject(hbwindow);
+	DeleteDC(hwindowCompatibleDC);
+	ReleaseDC(hwnd, hwindowDC);
+
+	Rect roi;
+	roi.x = x;
+	roi.y = y;
+	roi.width = w;
+	roi.height = h;
+
+	src = src(roi);
+
+	return src;
+}
+
+void main()
+{
+
+	HWND desk = GetDesktopWindow();
+	LPWSTR lpwstr = L"Test String";
+	while (desk != 0)
+	{/*
+		hwnd = GetWindow(hwnd, GW_CHILD);
+		GetWindowText(hwnd, lpwstr, 100);
+		wcout << lpwstr << endl;*/
+
+		Mat mat = hwnd2mat(desk, 0, 0, 320, 240);
+		imshow("Capture", mat);
+		waitKey(10); // need after imshow you can change wait time
+	}
+}
